58dc2ae90729bd9e590443188cc549bb7f94a25f
jeongminjee <joungmj9212@Nate.com>
2019-07-31 19:33:56 +0900

    22

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jeonghead.wsgi --log-file - 
--- jeong/settings.py
+++ jeong/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,14 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  SECRET_KEY = os.environ.get(‘DJANGO_SECRET_KEY’, ‘3z%p+-tok%ei8d78z0s+ks73g%+dz3%@ic@wr’)#!=2&d+)gbs-v'
+SECRET_KEY =  SECRET_KEY = os.environ.get(‘DJANGO_SECRET_KEY’, "3z%p+-tok%ei8d78z0s+ks73g%+dz3%@ic@wr’)#!=2&d+)gbs-v")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-DEBUG = bool(os.environ.get(‘DJANGO_DEBUG’, True))
-
-
-
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = ['*']
 
 
@@ -46,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,6 +82,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+db_from_env = dj_database_url.config(conn_max_age = 500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
